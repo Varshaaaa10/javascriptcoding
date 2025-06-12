7e4514804720dff76e3e7f7beca04b7acaf1e075
varsha <varshakalyadapu6@gmail.com>
2025-06-12 07:49:47 -0700

    initian code

---
+++ conditional.js
@@ -0,0 +1,29 @@
+if (condition) {
+  // code to run if condition is true
+} else {
+  // code to run if condition is false
+}
+Deepak Yadav
+19:16
+if (condition) {
+  // code to run if condition is true
+} else {
+  // code to run if condition is false
+}
+
+
+switch (expression) {
+  case value1:
+    // code to run if expression === value1
+    break;
+
+  case value2:
+    // code to run if expression === value2
+    break;
+
+  default:
+    // code to run if no case matches
+}
+
+
+condition ? expressionIfTrue : expressionIfFalse;
